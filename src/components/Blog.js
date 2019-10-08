118382d136d9cdc4086e2c09e0286bb874675a8a
divantso <divantso@cisco.com>
2019-10-08 17:08:55 +0200

    Add functionlity for infinite scroll on blog page

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -2,8 +2,44 @@ import React, { Component } from 'react';
 import {blogs} from '../helpers/blogs';
 
 export default class Blog extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: 0,
+      prevY: 0,
+      loading: false,
+      posts: []
+    }
+  }
+
+  // componentDidMount() {
+  //   const config = {
+  //     root: document.querySelector('.app-div')
+  //   }
+  //   const observer = new IntersectionObserver(
+  //     this.handleObserver.bind(this),
+  //     config
+  //   );
+  //   observer.observe(this.loadingRef)
+  // }
+
+  handleObserver(entities, observer) {
+    const y = entities[0].boundingClientDirect.y;
+    if(this.state.prevY > y) {
+      const lastPost = this.state.posts[this.state.posts.length - 1];
+      const curPage = lastPost.id;
+      this.props.getBlogPosts(curPage);
+      this.setState({page: curPage});
+    }
+    this.setState({prevY: y});
+  }
 
   render() {
+    const loadingCSS = {
+      width: '80vw',
+      height: '30px'
+    }
+    const loadingTextCSS = { display: this.state.loading ? 'block' : 'none' };
     return (
       <div className="blogs-container">
         <div className="card">
@@ -20,6 +56,9 @@ export default class Blog extends Component {
             </div>
           </div>
         ))}
+        <div style={loadingCSS} ref={loadingRef=>this.loadingRef = loadingRef}>
+          <span style={loadingTextCSS}>Loading...</span>
+        </div>
       </div>
     )
   }
