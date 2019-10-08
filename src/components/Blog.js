c77cde1935d771ea0e47d875163e0c7a06382604
divantso <divantso@cisco.com>
2019-10-08 16:29:49 +0200

    Create fake blogs persistence and display them on Blogs page

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,10 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {blogs} from '../helpers/blogs';
 
 export default class Blog extends Component {
+
   render() {
     return (
-      <div>
-        <h2>Blog</h2>
+      <div className="blogs-container">
+        <div className="card">
+          <div className="card-body">
+            <h2>Blog</h2>
+          </div>
+        </div>
+        { blogs.map((blog, i) => (
+          <div key={i} className="card">
+            <div className="card-body">
+              <h5 className="card-title">{blog.title}</h5>
+              <p className="card-text">{blog.content}</p>
+              <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        ))}
       </div>
     )
   }
