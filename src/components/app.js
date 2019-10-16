7338f85849cee830ec4cb109697c6f568d20a4d5
divantso <divantso@cisco.com>
2019-10-16 20:58:09 +0200

    Add routes for new BlogPost creation, add history object to use it outside of components

--- src/components/app.js
+++ src/components/app.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {Redirect} from 'react-router-dom';
 import Header from './header';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {Router, Route} from 'react-router-dom';
+import history from '../helpers/history';
 import Work from './content/Work';
 import Blog from './content/Blog';
+import NewPost from './content/NewPost';
+import NewPostReview from './content/NewPostReview';
 import About from './content/About';
 import Login from './authentication/Login';
 import Register from './authentication/Register';
@@ -12,15 +15,18 @@ import Register from './authentication/Register';
 class App extends Component {
   render() {
     return (
-      <Router>
+      <Router history={history}>
         <Route exact path="/login" component={Login}/>
         <Route exact path="/signup" component={Register}/>
         { (!localStorage.getItem('isLoggedIn')) 
           ? (<Redirect to="login"/>)
           : (<div className="app-div">
             <Header/>
+              <Route path="/" exact component={Work}/>
               <Route path="/work" component={Work}/>
-              <Route path="/blog" component={Blog}/>
+              <Route path="/blog" exact component={Blog}/>
+              <Route path="/blog/new" exact component={NewPost}/>
+              <Route path="/blog/new/review" exact component={NewPostReview}/>
               <Route path="/about" component={About}/>
             </div>
           )
